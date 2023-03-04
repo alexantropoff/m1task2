0a7423fa0239e55b845221190e6011ad77b49b05
Alex Antropoff <alant@idc.md>
2023-03-04 09:36:18 +0200

    Initial Commit

---
+++ m1t2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  m1t2
+//
+//  Created by Alex Antropoff on 04.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
